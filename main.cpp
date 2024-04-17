7864283524b615da98639cbdc37b595c88d149e6
Nguyen Duc Tam <nguyndantdm6@gmail.com>
2024-04-17 09:09:30 +0700

    fix leap year check

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ public:
         if (month < 1 || month > 12) {
             throw std::invalid_argument("Month must be between 1 and 12");
         }
-        if (month == 2 && day > 28) {
+        if (month == 2 && ((year % 4 != 0 && day > 28) || (year % 4 == 0 && day > 29))) {
             throw std::invalid_argument("Day must be between 1 and 28");
         } else if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
             throw std::invalid_argument("Day must be between 1 and 30");
